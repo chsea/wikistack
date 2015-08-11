cb54fc8af4d3536053e5857d6567694bb2a756c1
Chelsea Du <chsea.du@gmail.com>
2015-08-11 17:57:42 -0400

    added routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var swig = require('swig');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,12 +8,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var wiki = require('./routes/wiki');
 
 var app = express();
 
 // view engine setup
+app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/wiki/', wiki);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,6 +48,8 @@ if (app.get('env') === 'development') {
       error: err
     });
   });
+
+  swig.setDefaults({ cache: false });
 }
 
 // production error handler
@@ -55,6 +61,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
---
+++ routes/wiki.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('got to GET /wiki/');
+});
+
+router.post('/', function(req, res, next) {
+  res.send('got to POST /wiki/');
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('addpage');
+});
+
+module.exports = router;
